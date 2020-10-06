9ceb42615055624521b16b9c225793a9c587403b
ILiekMelons <49169907+ILiekMelons@users.noreply.github.com>
2020-10-06 20:18:26 +0100

    git

---
+++ commands/test.py
@@ -0,0 +1,12 @@
+import discord
+
+
+class Main:
+    command = "test"
+
+    def __init__(self, client: discord.Client):
+        self.CLIENT = client
+
+    async def handle(self, message: discord.Message):
+        await message.channel.send('hello')
+        return True
