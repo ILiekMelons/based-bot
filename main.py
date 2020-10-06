c582c2f320d3910034dad8a132954617d672098f
ILiekMelons <49169907+ILiekMelons@users.noreply.github.com>
2020-10-06 20:16:20 +0100

    tears

---
+++ main.py
@@ -0,0 +1,37 @@
+import importlib.util
+import os
+import discord
+
+TOKEN = os.environ['TOKEN']
+
+
+class Client(discord.Client):
+    COMMANDS = set()
+    HANDLERS = set()
+    PREFIX = "x!"
+
+    def __init__(self, *args, loop=None, **kwargs):
+        super().__init__(*args, loop=loop, **kwargs)
+
+        for module in os.listdir(os.path.dirname("commands/")):
+            if module == '__init__.py' or module[-3:] != '.py':
+                continue
+            module = importlib.import_module("commands." + module.replace(".py", ""))
+            mclass = module.Main(self)
+            self.COMMANDS.add(mclass)
+        print("loaded commands")
+
+    async def on_message(self, message: discord.Message):
+        if message.author == self.user or message.content[0:len(self.PREFIX)] != self.PREFIX:
+            return
+
+        command = message.content.replace(self.PREFIX, "").split()[0]
+        available = [x for x in self.COMMANDS if x.command == command]
+
+        for handler in available:
+            result = await handler.handle(message)
+            if result:
+                break
+
+
+Client().run(TOKEN)
