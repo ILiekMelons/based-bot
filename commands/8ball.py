14c01ad34a0022020373028a7837c399b2a7eabb
ILiekMelons <49169907+ILiekMelons@users.noreply.github.com>
2020-10-06 23:13:22 +0100

    8ball

---
+++ commands/8ball.py
@@ -0,0 +1,35 @@
+import discord
+import random
+from classes.BaseCommand import Command
+
+responses = [
+        "As I see it, yes",
+        "Ask again later",
+        "Better not tell you now",
+        "Cannot predict now",
+        "Concentrate and ask again",
+        "Don’t count on it",
+        "It is certain",
+        "It is decidedly so",
+        "Most likely",
+        "My reply is no",
+        "My sources say no",
+        "Outlook good",
+        "Outlook not so good",
+        "Reply hazy, try again",
+        "Signs point to yes",
+        "Very doubtful",
+        "Without a doubt",
+        "Yes",
+        "Yes definitely",
+        "You may rely on it"
+]
+
+
+class Main(Command):
+    command = "test"
+    description = "testing testing"
+    usage = "test"
+
+    async def handle(self, message: discord.Message):
+        await message.channel.send(f'{message.author.mention}: {random.choice(responses)}')
