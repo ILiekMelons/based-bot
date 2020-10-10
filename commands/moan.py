c15adc18d18240c7ecdd5c5b666033e5f2283ef8
ILiekMelons <49169907+ILiekMelons@users.noreply.github.com>
2020-10-10 09:24:44 +0100

    h

--- commands/moan.py
+++ commands/moan.py
@@ -4,7 +4,7 @@ from classes.BaseRoleplayCommand import BaseRoleplayCommand
 class Main(BaseRoleplayCommand):
     command = "moan"
     description = "pervert"
-    usage = "moan" <user>"
+    usage = "moan <user>"
     no_mention_response = "*moans*"
     self_response = ":smirk:"
     bot_response = "*moans*"
