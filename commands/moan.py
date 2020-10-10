ad9ac996150265cbc3c526b90a088d773cfb30d8
ILiekMelons <49169907+ILiekMelons@users.noreply.github.com>
2020-10-10 01:14:03 +0100

    suicide

--- commands/moan.py
+++ commands/moan.py
@@ -1,8 +1,11 @@
-from classes.BaseResponseCommand import BaseResponseCommand
+from classes.BaseRoleplayCommand import BaseRoleplayCommand
 
 
-class Main(BaseResponseCommand):
+class Main(BaseRoleplayCommand):
     command = "moan"
     description = "pervert"
-    usage = "moan"
-    response = "*moans*"
+    usage = "moan" <user>"
+    no_mention_response = "*moans*"
+    self_response = ":smirk:"
+    bot_response = "*moans*"
+    action = "*{mentioned} moans*"
