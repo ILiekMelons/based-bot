65fc97b4bcf632852bd0bb298ffeee6ccc537a76
ILiekMelons <49169907+ILiekMelons@users.noreply.github.com>
2020-10-10 01:05:35 +0100

    suicide

---
+++ commands/moan.py
@@ -0,0 +1,8 @@
+from classes.BaseResponseCommand import BaseResponseCommand
+
+
+class Main(BaseResponseCommand):
+    command = "moan"
+    description = "pervert"
+    usage = "moan"
+    response = "*moans*"
